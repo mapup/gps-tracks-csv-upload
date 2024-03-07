57c430ccd9d85c6b14e7825fc6f2a726b145942c
Vishal Balaji D <vishalbd@mapup.ai>
2024-03-07 21:58:24 +0530

    refactor: fix API key and formatting issues

--- gps-tracks-csv-upload.py
+++ gps-tracks-csv-upload.py
@@ -1,11 +1,13 @@
 import json
 from time import sleep
 import requests
+import os
 
 from urllib.parse import urlencode
 
-TOLLGURU_API_KEY = "YOUR_TOLLGURU_API_KEY" # API key for Tollguru
-TOLLGURU_API_URL = f"https://apis.tollguru.com/toll/v2" # Base URL for TollGuru Toll API
+TOLLGURU_API_KEY = os.environ.get("TOLLGURU_API_KEY")
+TOLLGURU_API_URL = "https://apis.tollguru.com/toll/v2"
+
 GPS_UPLOAD_ENDPOINT = "gps-tracks-csv-upload"
 GPS_DOWNLOAD_ENDPOINT = "gps-tracks-csv-download"
 
@@ -18,11 +20,12 @@ PARAMETERS = {
     },
 }
 
+
 # Upload CSV file - You can use the CSV format file as shown in the table below.
 def gps_tracks_csv_upload(is_async: bool = False):
-    payload = "lat-long-france-sample.csv"
+    payload = "Sample-GPS-tracks/france-GPS-Track-sample.csv"
 
-    url = f"{TOLLGURU_API_URL}/{GPS_UPLOAD_ENDPOINT}?${urlencode({ **PARAMETERS, "isAsync": is_async })}"
+    url = f"{TOLLGURU_API_URL}/{GPS_UPLOAD_ENDPOINT}?{urlencode({**PARAMETERS['vehicle'], 'isAsync': is_async})}"
 
     headers = {"x-api-key": TOLLGURU_API_KEY, "Content-Type": "text/csv"}
 
@@ -33,7 +36,9 @@ def gps_tracks_csv_upload(is_async: bool = False):
 
 
 # When isAsync is set to true, use this funcation to get the result by passing the return value from the gps_tracks_csv_upload
-def gps_tracks_csv_download(response_from_gps_tracks_csv_upload, retry: int = 5, delay: int = 5):
+def gps_tracks_csv_download(
+    response_from_gps_tracks_csv_upload, retry: int = 5, delay: int = 5
+):
     count = retry  # number of retries before stoping
     status = "ERROR"
     response = None
@@ -54,5 +59,9 @@ def gps_tracks_csv_download(response_from_gps_tracks_csv_upload, retry: int = 5,
     return response
 
 
-# print(gps_tracks_csv_upload())
-# print(gps_tracks_csv_download(response_from_gps_tracks_csv_upload=gps_tracks_csv_upload(is_async=True)))
+print(gps_tracks_csv_upload())
+print(
+    gps_tracks_csv_download(
+        response_from_gps_tracks_csv_upload=gps_tracks_csv_upload(is_async=True)
+    )
+)
